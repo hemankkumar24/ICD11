09e16880d2ae7ad85e43f7fd9514ed41c0a32ef9
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-08 13:56:55 +0530

    changes: routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+import Landing from './pages/Landing';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes> 
+        <Route path="/" element={ <Landing /> } />
+        <Route path="/dashboard" element={  <Dashboard /> } />
+      </Routes>
+    </div>
   )
 }
 
