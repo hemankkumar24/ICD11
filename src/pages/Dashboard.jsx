e4cb229042fe641149f167c0d17b3924009d867a
hemankkumar24 <hemankkumar24@gmail.com>
2026-03-08 14:39:51 +0530

    update: font + form layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='px-75 bg-stone-100 w-full h-screen '>
+        <div className='bg-stone-50 w-full h-screen shadow-lg'>
+            {/* Progress Bar */}
+            <div className='flex justify-around text-sm'>
+                <div className='flex'>
+                    Code Matching
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
